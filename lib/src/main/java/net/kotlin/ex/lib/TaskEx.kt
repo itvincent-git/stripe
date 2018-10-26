02047c7e1d7ca6ec82e9f45e0686092fbf2b0994
Vincent Zhong <itvincent@gmail.com>
2018-10-26 15:21:45 +0800

    add comment

--- lib/src/main/java/net/kotlin/ex/lib/TaskEx.kt
+++ lib/src/main/java/net/kotlin/ex/lib/TaskEx.kt
@@ -5,6 +5,11 @@ import java.util.concurrent.atomic.AtomicBoolean
 /**
  * Created by zhongyongsheng on 2018/10/25.
  */
+
+/**
+ * 只运行一次的任务
+ * @param runBlock 任务
+ */
 class RunOnceTask(private val runBlock: () -> Unit) {
     private val hasRun = AtomicBoolean(false)
 
