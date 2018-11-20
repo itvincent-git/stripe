bd6416c13ffdad931c50c5bde7ecadcc5f854dd2
Vincent Zhong <itvincent@gmail.com>
2018-11-20 14:42:31 +0800

    加注释

--- app/src/main/java/net/kotlin/ex/sample/LifecycleActivity.kt
+++ app/src/main/java/net/kotlin/ex/sample/LifecycleActivity.kt
@@ -11,10 +11,12 @@ class LifecycleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lifecycle)
 
+        //延迟3秒执行；activity onDestory时取消
         runInMainThread(lifecycleOwner = this, delay = 3000) {
             showToast(this, "runInMainThread")
         }
 
+        //延迟4秒执行；activity onPause时取消
         runInMainThread(lifecycleOwner = this, delay = 4000, cancelWhenEvent = Lifecycle.Event.ON_PAUSE) {
             showToast(this, "runInMainThread cancel when pause")
         }
