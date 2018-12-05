414787d77f8f9f12ae93d51c4513971bdf4772ee
Vincent Zhong <itvincent@gmail.com>
2018-12-05 22:48:15 +0800

    add coroutineScope; add log

--- app/src/main/java/net/kotlin/ex/sample/coroutines/CoroutineScopeActivity.kt
+++ app/src/main/java/net/kotlin/ex/sample/coroutines/CoroutineScopeActivity.kt
@@ -2,9 +2,7 @@ package net.kotlin.ex.sample.coroutines
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import net.kotlin.ex.lib.lifecycleScope
 import net.kotlin.ex.sample.R
 import net.kotlin.ex.sample.util.debugLog
@@ -23,11 +21,12 @@ class CoroutineScopeActivity : AppCompatActivity() {
         }
     }
 
-    suspend fun doSomething() {
+    suspend fun doSomething() = coroutineScope {
+        debugLog("this will run in DefaultDispatcher")
         // launch ten coroutines for a demo, each working for a different time
         repeat(10) { i ->
             //this job will cancel when activity onDestory
-            lifecycleScope.launch {
+            launch {
                 delay((i + 1) * 500L) // variable delay 200ms, 400ms, ... etc
                 debugLog("Coroutine $i is done")
             }
