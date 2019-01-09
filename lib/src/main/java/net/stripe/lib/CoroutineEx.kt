afd3f9e8b4cfa7cc27efd05dba6ebd04ab5c8943
Vincent Zhong <itvincent@gmail.com>
2019-01-09 15:35:25 +0800

    remove CoroutineLifecycle

--- lib/src/main/java/net/stripe/lib/CoroutineEx.kt
+++ lib/src/main/java/net/stripe/lib/CoroutineEx.kt
@@ -19,61 +19,6 @@ val LoggingExceptionHandler = CoroutineExceptionHandler { context, throwable ->
 }
 
 /**
- * 协程支持生命周期
- * Created by zhongyongsheng on 2018/9/20.
- */
-class CoroutineLifecycle {
-    var mLastEvent: Lifecycle.Event = Lifecycle.Event.ON_ANY
-    var mTargetEvent: Lifecycle.Event = Lifecycle.Event.ON_ANY
-
-    fun <T> observe(lifecycleOwner: LifecycleOwner, deferred: Deferred<T>) {
-        lifecycleOwner.lifecycle.addObserver(object: LifecycleObserver {
-            @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
-            fun onStateChanged(owner:LifecycleOwner, event: Lifecycle.Event) {
-                if (mLastEvent == Lifecycle.Event.ON_ANY) {
-                    mLastEvent = event
-                    when (mLastEvent) {
-                        Lifecycle.Event.ON_CREATE -> mTargetEvent = Lifecycle.Event.ON_DESTROY
-                        Lifecycle.Event.ON_START -> mTargetEvent = Lifecycle.Event.ON_STOP
-                        Lifecycle.Event.ON_RESUME -> mTargetEvent = Lifecycle.Event.ON_PAUSE
-                    }
-                }
-                if (event == mTargetEvent) {
-                    //cancel the coroutine
-                    deferred.cancel()
-                }
-                if (owner.lifecycle.currentState == Lifecycle.Event.ON_DESTROY) {
-                    owner.lifecycle.removeObserver(this)
-                    return
-                }
-            }
-        })
-    }
-}
-
-/**
- * 执行异步任务并绑定生命周期
- */
-fun <T, S: CoroutineScope> S.asyncWithLifecycle(lifecycleOwner: LifecycleOwner,
-                                                context: CoroutineContext = EmptyCoroutineContext,
-                                                start: CoroutineStart = CoroutineStart.DEFAULT,
-                                                block: suspend CoroutineScope.() -> T): Deferred<T> {
-    val job = appScope.async(context = context, start = start, block = block)
-    CoroutineLifecycle().observe(lifecycleOwner, job)
-    return job
-}
-
-/**
- * 为协程block绑定lifecycle生命周期
- */
-inline fun <T, S: CoroutineScope> S.bindLifecycle(lifecycleOwner: LifecycleOwner,
-                                                  block: CoroutineScope.() -> Deferred<T>): Deferred<T> {
-    val job = block.invoke(this)
-    CoroutineLifecycle().observe(lifecycleOwner, job)
-    return job
-}
-
-/**
  * 等待await的结果，如果出现异常或出现超时，则返回为null
  * @param timeout 超时时长，默认为0，则不设置超时
  * @param unit 时长单位
