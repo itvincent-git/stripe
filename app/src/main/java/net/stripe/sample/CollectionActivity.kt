ea6e869e0cd6eb4adbe96627897e0714369cecae
Vincent Zhong <itvincent@gmail.com>
2019-01-14 18:23:26 +0800

    add collection test

--- app/src/main/java/net/stripe/sample/CollectionActivity.kt
+++ app/src/main/java/net/stripe/sample/CollectionActivity.kt
@@ -2,6 +2,7 @@ package net.stripe.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_collection.*
 import net.stripe.lib.random
 import net.stripe.sample.util.debugLog
 
@@ -14,19 +15,25 @@ class CollectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collection)
 
-        val data = mutableListOf(1, 3, 4, 5, 8, 9, 11)
-        data.retainAll { it % 2 == 0 }.let { debugLog("保留偶数 $data") }
-        data += listOf(1, 3, 4, 5, 8, 9, 11)
-        data.removeAll { it % 2 == 0 }.let { debugLog("移除偶数 $data") }
+        plus_btn.setOnClickListener {
+            val data = mutableListOf(1, 3, 4, 5, 8, 9, 11)
+            data.retainAll { it % 2 == 0 }.let { debugLog("保留偶数 $data") }
+            data += listOf(1, 3, 4, 5, 8, 9, 11)
+            data.removeAll { it % 2 == 0 }.let { debugLog("移除偶数 $data") }
 
-        data += 66
-        data.let { debugLog("增加66 $it") }
-        data += listOf(12, 13)
-        data.let { debugLog("增加12,13 $it") }
-        data -= 66
-        data.let { debugLog("删除66 $it") }
+            data += 66
+            data.let { debugLog("增加66 $it") }
+            data += listOf(12, 13)
+            data.let { debugLog("增加12,13 $it") }
+            data -= 66
+            data.let { debugLog("删除66 $it") }
 
 
-        debugLog("随机从数组取一个数： ${data.random()}")
+            debugLog("随机从数组取一个数： ${data.random()}")
+        }
+
+        array_btn.setOnClickListener {
+            debugLog("LongArray.toString:" + arrayOf(1L, 2L, 3L).joinToString())
+        }
     }
 }
