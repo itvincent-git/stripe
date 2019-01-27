c908b794dbae1bd209cd0b844d1d41cafad9f5c6
itvincent-git <itvincent@gmail.com>
2019-01-27 22:57:36 +0800

    add getSystemProp return Int

--- lib/src/main/java/net/stripe/lib/SystemEx.kt
+++ lib/src/main/java/net/stripe/lib/SystemEx.kt
@@ -5,6 +5,9 @@ package net.stripe.lib
  * Created by zhongyongsheng on 2019/1/17.
  */
 
+/**
+ * set system property
+ */
 fun setSystemProp(propertyName: String, propertyValue: String?) {
     try {
         System.setProperty(propertyName, propertyValue)
@@ -12,10 +15,30 @@ fun setSystemProp(propertyName: String, propertyValue: String?) {
     }
 }
 
+/**
+ * get system property
+ */
 fun getSystemProp(propertyName: String): String? =
-    try {
-        System.getProperty(propertyName)
-    } catch (e: SecurityException) {
-        null
-    }
+        try {
+            System.getProperty(propertyName)
+        } catch (e: SecurityException) {
+            null
+        }
 
+/**
+ * get system property as Int
+ */
+fun getSystemProp(
+        propertyName: String,
+        defaultValue: Int,
+        minValue: Int = 1,
+        maxValue: Int = Int.MAX_VALUE
+): Int {
+    val value = getSystemProp(propertyName) ?: return defaultValue
+    val parsed = value.toIntOrNull()
+            ?: error("System property '$propertyName' has unrecognized value '$value'")
+    if (parsed !in minValue..maxValue) {
+        error("System property '$propertyName' should be in range $minValue..$maxValue, but is '$parsed'")
+    }
+    return parsed
+}
