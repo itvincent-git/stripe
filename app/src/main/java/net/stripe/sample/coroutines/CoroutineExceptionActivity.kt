7fe4938b905db6f786a6c8357e326d3093e020e8
Vincent Zhong <zhongyongsheng@yy.com>
2019-12-28 17:46:48 +0800

    测试协程的异常

--- app/src/main/java/net/stripe/sample/coroutines/CoroutineExceptionActivity.kt
+++ app/src/main/java/net/stripe/sample/coroutines/CoroutineExceptionActivity.kt
@@ -3,9 +3,12 @@ package net.stripe.sample.coroutines
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.channels.consumeEach
 import net.stripe.lib.appScope
 import net.stripe.lib.lifecycleScope
 import net.stripe.sample.R
+import net.stripe.sample.util.debugLog
 import net.stripe.sample.util.errorLog
 import java.lang.RuntimeException
 
@@ -17,9 +20,24 @@ class CoroutineExceptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine_exception)
-        loadData()
-        loadDataLifecycle()
-        loadDataInAppScope()
+//        loadData()
+//        loadDataLifecycle()
+//        loadDataInAppScope()
+//        loadDataInGlobal()
+        actor.offer(System.currentTimeMillis())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //不加这判断就会崩溃
+        if (!actor.isClosedForSend) {
+            actor.offer(System.currentTimeMillis())
+        }
+
+        //在destory时调用lifecycleScope并不会崩溃，代码也并不会执行
+        lifecycleScope.launch {
+            debugLog("test onDestroy")
+        }
     }
 
 //    val job: Job = Job()//子协程的异常或取消，会停止父协程的运行，直接抛出子协程的异常，导致崩溃
@@ -59,6 +77,25 @@ class CoroutineExceptionActivity : AppCompatActivity() {
             }.await()
         } catch (e: Exception) {
             errorLog("loadDataInAppScope", e)
+        }
+    }
+
+    //使用GlobalScope能捕获到异常，但仍然会崩溃
+    private fun loadDataInGlobal() = GlobalScope.launch {
+        try {
+            async {
+                throw RuntimeException()
+            }.await()
+        } catch (e: Exception) {
+            errorLog("loadDataInGlobal", e)
+        }
+    }
+
+    //使用actor接收时，出现崩溃
+    private val actor = lifecycleScope.actor<Long> {
+        consumeEach {
+            //异常会被lifecycleScope中定义的ExceptionHandler处理
+            throw ConcurrentModificationException()
         }
     }
 }
